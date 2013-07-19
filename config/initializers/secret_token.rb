88a5a8be7bbba36bed209babb45e7ff0ea6c7ebd
MikkaH <mikkah@mikkah-desktop.(none)>
2013-07-19 14:26:46 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bricolo::Application.config.secret_token = '4f67faecba6004e1b47ffa43c308b7e58cc1a4dda8b8b24077e7097e668e381dbd478944e11aeaf52d9e04ee27ec980813fece2ad49ab68d5d4e56299aba0885'
